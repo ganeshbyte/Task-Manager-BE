1ddf79f47c619c0494eb037495f0d40e02f313f6
ganeshbyte <gc930954@gmail.com>
2024-09-29 16:16:50 +0530

    feat(NA): validation add for task schema

--- models/task.model.js
+++ models/task.model.js
@@ -3,9 +3,13 @@ import mongoose from "mongoose";
 const taskSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: [true, "name is required"],
+    trim: true,
+    maxLength: [20, "name can not be more than 20 character"],
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
